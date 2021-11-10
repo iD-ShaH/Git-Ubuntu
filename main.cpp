dc65a30429c88ad8727ed072747caf281ab82401
shahlar <vip.shahlar199881@gmail.com>
2021-11-10 19:24:46 +0200

    added DZ1 DZ2 DZ3 main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// using namespace std - в принципе можно было бы использовать)
+int main ()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+/*
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+	setlocale (LC_ALL, "ru");
+	cout << "Привет, всем кто ходит с нами под одним солнцем!" << endl;
+	return 0;
+}
+
+*/
+}
